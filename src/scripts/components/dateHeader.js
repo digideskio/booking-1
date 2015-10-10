ec8a2d67bea4345beaa5575d9e57d3a0cc628c09
JackChan <jack5241027@gmail.com>
2015-10-10 21:23:16 +0800

    [Doing] refactor let the dateTable control date and the header and eventWrap just for show

--- src/scripts/components/dateHeader.js
+++ src/scripts/components/dateHeader.js
@@ -6,69 +6,14 @@ var DateStore = require('../stores/DateStore.js');
 var DateControl = React.createClass({
     getInitialState: function() {
         var state = {
-            todayIndex : this.dateToString(this.props.date),
-            thisWeek : this.nextFewDay(this.props.date,this.props.showDay)
+            thisWeek : this.props.thisWeek
         }
         return state;
-    },
-    componentWillMount: function(){
-        let datesIndex = []
-        this.setState({
-        });
-        this.state.thisWeek.map(function(e,i){
-            datesIndex.push(this.dateToString(e));   
-        },this);
-        AppActions.saveDates(datesIndex);
-    },
-    shouldComponentUpdate : function(nextProps, nextState){
-        var nextFirstIndex = this.dateToString(nextProps.date);
-        var nextLastindex = this.dateToString(nextState.thisWeek[6]);
-        var orgWeek = DateStore.getDatesIndex();
-        if( (nextFirstIndex - this.state.todayIndex) >= 0){
-            if(orgWeek.indexOf(nextLastindex)==-1){
-                orgWeek.push(nextLastindex);
-            }
-            return true;
-        }else{
-            return false;
-        }
-    },
+    },    
     componentWillReceiveProps:function(props){
         this.setState({
-            thisWeek : this.nextFewDay(props.date,props.showDay)
+            thisWeek : props.thisWeek
         })
-    },
-    componentWillUpdate : function(){
-
-    },      
-    dateToString : function(date){
-        let space = "0";
-        if(date.date.toString().length != 1){
-            space = "";
-        }
-        return date.year.toString() + date.month + space + date.date;
-    },    
-    nextFewDay : function(first,showDay){
-        var dates = [];
-        var firstDay = {
-            year : first.year,
-            month : first.month,
-            date : first.date,
-            today : first.today.replace("星期", "(") + ")"
-        };
-        dates.push(firstDay);
-        if(showDay){
-            for(let loopIndex = 1;loopIndex<7;loopIndex++){
-                dates.push(
-                    showDay({
-                        today : dates[loopIndex - 1],
-                        action : "next"
-                    })
-                );
-                dates[loopIndex].today = dates[loopIndex].today.replace("星期", "(") + ")";
-            }
-        }
-        return dates;
     },
 	render: function () {
         var dateHtml = this.state.thisWeek.map(function(e,index){
--- src/scripts/components/dateTable.js
+++ src/scripts/components/dateTable.js
@@ -1,21 +1,98 @@
 var React = require('react');
 var AppActions = require('../actions/AppActions.js');
 var UserStore = require('../stores/UserStore.js');
+var DateStore = require('../stores/DateStore.js');
 var DateHeader = require('../components/dateHeader.js');
 var DateEventWrap = require('../components/dateEventWrap.js');
 
 var DateControl = React.createClass({
+    getInitialState: function() {
+        var state = {
+            todayIndex : this.dateToString(this.props.date),
+            thisWeek : this.getNextSixDate(this.props.date,this.props.showDay),
+            thisWeekIndex : []
+        }
+        return state;
+    },
+    componentWillMount: function(){
+        let datesIndex = []
+        this.state.thisWeek.map(function(e,i){
+            datesIndex.push(this.dateToString(e));   
+        },this);
+        this.setState({
+            thisWeekIndex : datesIndex
+        });
+        AppActions.saveDates(datesIndex);
+    },
+    shouldComponentUpdate : function(nextProps, nextState){
+        var nextFirstIndex = this.dateToString(nextProps.date);
+        if( (nextFirstIndex - this.state.todayIndex) >= 0){
+            return true;
+        }else{
+            return false;
+        }
+    },
+    componentWillReceiveProps:function(nextProps){
+        var nextLastindex = this.dateToString(this.getNextSixDate(nextProps.date,nextProps.showDay)[6]);
+        var orgWeek = DateStore.getDatesIndex();
+        if(orgWeek.indexOf(nextLastindex)==-1){
+            orgWeek.push(nextLastindex);
+        }
+        this.setState({
+            thisWeek : this.getNextSixDate(nextProps.date,nextProps.showDay),
+            thisWeekIndex : this.getNextSixIndex(this.dateToString(nextProps.date))
+        });
+    },  
+    dateToString : function(date){
+        let monthSpace = "0";
+        let dateSpace = "0";
+        if(date.month.toString().length != 1){
+            monthSpace = "";
+        }
+        if(date.date.toString().length != 1){
+            dateSpace = "";
+        }
+        return date.year.toString() + monthSpace + date.month + dateSpace + date.date;
+    }, 
+    getNextSixDate : function(first,showDay){
+        var dates = [];
+        var firstDay = {
+            year : first.year,
+            month : first.month,
+            date : first.date,
+            today : first.today.replace("星期", "(") + ")"
+        };
+        dates.push(firstDay);
+        if(showDay){
+            for(let loopIndex = 1;loopIndex<7;loopIndex++){
+                dates.push(
+                    showDay({
+                        today : dates[loopIndex - 1],
+                        action : "next"
+                    })
+                );
+                dates[loopIndex].today = dates[loopIndex].today.replace("星期", "(") + ")";
+            }
+        }
+        return dates;
+    },
+    getNextSixIndex: function(index){
+        var firstIndex = DateStore.getDatesIndex().indexOf((index));
+        var lastIndex = firstIndex + 7;
+        var currentIndex = firstIndex - DateStore.getDatesIndex().length;
+        return DateStore.getDatesIndex().slice( currentIndex, lastIndex );
+    },   
 	render: function () {
-	    return (
+        return (
         <div className="topContainerDate">
             <table className="dateTable" cellPadding="0" cellSpacing="0">
                 <tbody>
-                    <DateHeader date = {this.props.date} showDay = {this.props.showDay}/>
-                    <DateEventWrap date = {this.props.date} />
+                    <DateHeader thisWeek = {this.state.thisWeek} />
+                    <DateEventWrap thisWeekIndex = {this.state.thisWeekIndex} />
                 </tbody>
             </table>
         </div> 
-    	);
+        );
 	}
 });
 
